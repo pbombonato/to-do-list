15a77c9c2bbc69424ab9708da1ab576d665c4a5d
pbombonato <pablobombonato14@gmail.com>
2023-03-09 17:18:43 -0300

    TrashButton test creation

--- frontend/src/components/TrashButton/index.jsx
+++ frontend/src/components/TrashButton/index.jsx
@@ -1,6 +1,6 @@
-import styles from './TrashButton.module.css'
+import styles from "./TrashButton.module.css";
 
-import {useContext} from "react";
+import { useContext } from "react";
 import axios from "axios";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -10,17 +10,21 @@ import { Context } from "../../context/taskContext";
 import { baseUrl } from "../../constants";
 
 function TrashButton({ task }) {
-  const { removeTask } = useContext(Context)
+  const { removeTask: removeTaskFromContext } = useContext(Context);
 
-  function remove(task) {
-    axios.delete(`${baseUrl}/${task.id}`).then((resp) => {
-      removeTask(task);
+  function removeTaskFromDB(task) {
+    axios.delete(`${baseUrl}/${task.id}`).then(() => {
+      removeTaskFromContext(task);
     });
   }
 
   return (
-    <div className={styles['div-btns']} >
-      <button className="btn" onClick={() => remove(task)}>
+    <div className={styles["div-btns"]}>
+      <button
+        aria-label="Delete task"
+        className="btn"
+        onClick={() => removeTaskFromDB(task)}
+      >
         <FontAwesomeIcon icon={faTrash} />
       </button>
     </div>
