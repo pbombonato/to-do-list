ea7aca8b59332135c2cae11a2f2236b8e821ce4b
pbombonato <pablobombonato14@gmail.com>
2023-03-10 15:52:24 -0300

    Fixing Footer tests

--- frontend/src/templates/Footer/Footer.spec.jsx
+++ frontend/src/templates/Footer/Footer.spec.jsx
@@ -1,16 +1,11 @@
-import { render, screen } from '@testing-library/react';
-import Footer from '.'
+import { render, screen } from "@testing-library/react";
+import Footer from ".";
 
-describe('<Footer/>', () => {
-  it('should render the footer text', () => {
-    render(<Footer />)
+describe("<Footer/>", () => {
+  it("should render the footer text", () => {
+    render(<Footer />);
 
-    expect.assertions(2)
-
-    const span = screen.getByText('Developed by .')
-    expect(span).toBeInTheDocument()
-
-    const strong = screen.getByText('Pablo Bombonato')
-    expect(strong).toBeInTheDocument()
-  })
-})
+    const footerText = screen.getByRole("contentinfo");
+    expect(footerText).toHaveTextContent(/Developed by Pablo Bombonato/i);
+  });
+});
